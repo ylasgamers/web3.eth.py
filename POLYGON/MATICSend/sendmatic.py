a6532be4311849d7980ee84573fdb816403b9170
ylasgamers <92687144+ylasgamers@users.noreply.github.com>
2023-07-28 20:15:00 +0700

    Add files via upload

---
+++ POLYGON/MATICSend/sendmatic.py
@@ -0,0 +1,81 @@
+from web3 import Web3, HTTPProvider
+import time
+import datetime 
+import threading
+import json
+import logging
+import asyncio
+import requests
+import time
+import os
+import sys
+import ctypes
+import pyperclip as pc
+bsc = "https://poly-rpc.gateway.pokt.network" #rpc polygon custom
+web3 = Web3(Web3.HTTPProvider(bsc))
+
+ctypes.windll.kernel32.SetConsoleTitleW("MATIC Sender")
+print('send matic to other address')
+
+#connecting web3
+if  web3.isConnected() == True:
+    print("web3 connected...\n")
+else :
+    print("error connecting please try again...")
+
+walletSender = web3.toChecksumAddress(input("Enter your address sender 0x...: "))
+#walletSender = '0x0' #sender
+walletSenderKey = input("Enter your privatekey sender abcde12345...: ")
+#walletSenderKey = 'abcde12345' #privatekey sender
+walletRecipient = web3.toChecksumAddress(input("Enter your address recipient 0x...: "))
+#walletRecipient = '0x0' #recipient 
+#gasAmount = 21000 #gas limit
+#gasPrice = 1 #gas price
+chainId = 137
+
+#Get balance account
+balance = web3.eth.get_balance(walletSender)
+balance_matic = web3.fromWei(balance,'ether')
+print('Your Balance' ,balance_matic, 'MATIC')
+amountToSendMATIC = web3.toWei(float(input("Enter amount of matic you want to send: ")), 'ether') #ex 1 / 0.1 / 0.001 / 0.0001 / 0.00001
+
+#get the nonce.  Prevents one from sending the transaction twice
+nonce = web3.eth.getTransactionCount(walletSender)
+
+#estimate gas price
+# estgasPrice = int(web3.eth.gas_price)
+# gasPrice = web3.fromWei(float(estgasPrice), 'ether')
+
+#estimate gas limit
+gasAmount = web3.eth.estimate_gas({
+    'chainId': chainId,
+    'nonce': nonce,
+    'to': walletRecipient,
+    'value': amountToSendMATIC
+    })
+
+#build a transaction in a dictionary
+tx = {
+    'chainId': chainId,
+    'nonce': nonce,
+    'to': walletRecipient,
+    'value': amountToSendMATIC, #web3.toWei(float(amountToSendMATIC), 'ether'),
+    'gas': gasAmount,
+    'gasPrice': web3.eth.gas_price #web3.toWei(gasPrice, 'gwei')
+    }
+
+#sign the transaction
+signed_tx = web3.eth.account.sign_transaction(tx, walletSenderKey)
+#send transaction
+tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+
+#get transaction hash
+txid = str(web3.toHex(tx_hash))
+print('Transaction Success TX-ID Copied To Clipboard')
+print('https://polygonscan.com/tx/'+txid)
+pc.copy('https://polygonscan.com/tx/'+txid)
+print('update current balance in 30 second...')
+time.sleep(30)
+print('Current Balance' ,balance_matic, 'MATIC')
+print('will close automatically in 30 second...')
+time.sleep(30)
