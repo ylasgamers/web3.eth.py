8c64be093d2aefe20f62ad18a8070b67ec47d572
ylasgamers <92687144+ylasgamers@users.noreply.github.com>
2023-08-04 22:02:52 +0700

    Add files via upload

---
+++ DeployPy/ERC721NFTStandart/deployerc721.py
@@ -0,0 +1,129 @@
+from solcx import compile_standard, install_solc
+import datetime 
+import threading
+import asyncio
+import requests
+import time
+import os
+import sys
+import ctypes
+import pyperclip as pc
+install_solc('0.8.0')
+
+with open("ERC721NFT.sol", "r") as file:
+    erc721nft_file = file.read()
+
+import json
+...
+
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"ERC721NFT.sol": {"content": erc721nft_file}},
+        "settings": {
+             "optimizer": {
+             "enabled": bool(True),
+             "runs": 200
+            },
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"] # output needed to interact with and deploy contract 
+                }
+            }
+        },
+    },
+    solc_version="0.8.0",
+)
+#print(compiled_sol)
+#with open("compiled_code.json", "w") as file:
+#json.dump(compiled_sol, file)
+
+# get bytecode
+# ERC20TOKEN = mean contract function you want deploy in sol code
+bytecode = compiled_sol["contracts"]["ERC721NFT.sol"]["ERC721NFT"]["evm"]["bytecode"]["object"]# get abi
+abi = json.loads(compiled_sol["contracts"]["ERC721NFT.sol"]["ERC721NFT"]["metadata"])["output"]["abi"]
+
+#set title
+ctypes.windll.kernel32.SetConsoleTitleW("Deploy ERC721NFT Standart On Blockchain Network")
+print('')
+print('Deploy ERC721NFT Standart On Blockchain Network With Python')
+print('This Support Tesnet & Mainnet Ethereum, Binance Smart Chain, Polygon')
+print('Polygon zkEVM, Arbitrum, Optimism, Avalanche, zkSync Era, & Base')
+print('You Need Gas Fee Depends On Your Choose Blockchain Network Like ETH/BNB/MATIC/OTHER')
+print('')
+
+# For connecting to web3
+from web3 import Web3
+#bsc = "https://bsc-testnet.publicnode.com" #rpc bsctesnet custom #you can find rpc on chainlist.org
+inputrpc = str(input("Input Url RPC/Node Blockchain Network : "))
+web3 = Web3(Web3.HTTPProvider(inputrpc))
+#chain_id = 97 #you can find chainid on chainlist.org
+chain_id = int(input("Input Chain ID Blockchain Network : "))
+
+#connecting web3
+if  web3.isConnected() == True:
+    print("Web3 Connected...\n")
+else :
+    print("Error Connecting Please Try Again...")
+
+address = web3.toChecksumAddress(input("Enter Your Address 0x...: "))
+private_key = input("Enter Your Privatekey abcde12345...: ")
+nftName_ = str(input("Enter NFT Name [Exampel CAT] : "))
+nftSymbol_ = str(input("Enter NFT Symbol [Example CT] : "))
+Contract = web3.eth.contract(abi=abi, bytecode=bytecode)
+# Get the number of latest transaction
+nonce = web3.eth.getTransactionCount(address)
+
+#Get balance account
+print('')
+def UpdateBalance():
+    balance = web3.eth.get_balance(address)
+    balance_bnb = web3.fromWei(balance,'ether')
+    print('Your Balance' ,balance_bnb, 'ETH/BNB/MATIC/OTHER')
+    
+UpdateBalance()
+
+#estimate gas limit contract
+gas_tx = Contract.constructor(nftName_, nftSymbol_).buildTransaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": web3.eth.gas_price,
+        "from": address,
+        "nonce": nonce
+    }
+)
+gasAmount = web3.eth.estimateGas(gas_tx)
+
+#calculate transaction fee
+print('')
+gasPrice = web3.fromWei(web3.eth.gas_price, 'gwei')
+Caclfee = web3.fromWei(gasPrice*gasAmount, 'gwei')
+print('Transaction Fee :' ,Caclfee, 'ETH/BNB/MATIC/OTHER')
+
+# build transaction
+transaction = Contract.constructor(nftName_, nftSymbol_).buildTransaction(
+    {
+        "chainId": chain_id,
+        "gas": gasAmount,
+        "gasPrice": web3.eth.gas_price,
+        "from": address,
+        "nonce": nonce
+    }
+)
+# Sign the transaction
+sign_transaction = web3.eth.account.sign_transaction(transaction, private_key)
+# Send the transaction
+transaction_hash = web3.eth.send_raw_transaction(sign_transaction.rawTransaction)
+# Wait for the transaction to be mined, and get the transaction receipt
+#get transaction hash
+txid = str(web3.toHex(transaction_hash))
+transaction_receipt = web3.eth.wait_for_transaction_receipt(transaction_hash)
+print('Transaction Success Contract deployed! TX-ID & Contract Address Copied To Clipboard')
+print('TX-ID : '+txid+ ' & ' 'Contract Address : '+transaction_receipt.contractAddress)
+pc.copy('TX-ID : '+txid+ ' & ' 'Contract Address : '+transaction_receipt.contractAddress)
+print('Update Current Balance In 30 Second...')
+time.sleep(30)
+print('')
+UpdateBalance() #get latest balance
+print('Will Close Automatically In 30 Second...')
+time.sleep(30)
---
+++ DeployPy/ERC721NFTStandart/mintnft.py
@@ -0,0 +1,102 @@
+from web3 import Web3, HTTPProvider
+import json
+import logging
+import datetime 
+import threading
+import asyncio
+import requests
+import time
+import os
+import sys
+import ctypes
+import pyperclip as pc
+
+#set title
+ctypes.windll.kernel32.SetConsoleTitleW("Mint ERC721NFT Standart On Blockchain Network")
+print('')
+print('Mint ERC721NFT Standart On Blockchain Network With Python')
+print('Mint Can Execute Only Owner Of The Contract Address')
+print('This Support Tesnet & Mainnet Ethereum, Binance Smart Chain, Polygon')
+print('Polygon zkEVM, Arbitrum, Optimism, Avalanche, zkSync Era, & Base')
+print('You Need Gas Fee Depends On Your Choose Blockchain Network Like ETH/BNB/MATIC/OTHER')
+print('')
+
+# For connecting to web3
+#bsc = "https://bsc-testnet.publicnode.com" #rpc bsctesnet custom #you can find rpc on chainlist.org
+inputrpc = str(input("Input Url RPC/Node Blockchain Network : "))
+web3 = Web3(Web3.HTTPProvider(inputrpc))
+#chain_id = 97 #you can find chainid on chainlist.org
+chain_id = int(input("Input Chain ID Blockchain Network : "))
+
+#connecting web3
+if  web3.isConnected() == True:
+    print("Web3 Connected...\n")
+else :
+    print("Error Connecting Please Try Again...")
+
+address = web3.toChecksumAddress(input("Enter Your Owner Address 0x...: "))
+private_key = input("Enter Your Privatekey abcde12345...: ")
+recipient = web3.toChecksumAddress(input("Enter Your Address Recipient 0x...: "))
+token_id = int(input("Enter Random NFT-ID EX 1/2/3/4/ETC : "))
+contract_address = web3.toChecksumAddress(input('Enter NFT Contract Address 0x...: '))
+uri_url = str(input("Enter URI URL ipfs IMAGE NFT : "))
+
+abi = json.loads('[{"inputs":[{"internalType":"string","name":"nftName_","type":"string"},{"internalType":"string","name":"nftSymbol_","type":"string"}],"stateMutability":"payable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"_owner","type":"address"},{"indexed":true,"internalType":"address","name":"_approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"_tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"_owner","type":"address"},{"indexed":true,"internalType":"address","name":"_operator","type":"address"},{"indexed":false,"internalType":"bool","name":"_approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"_from","type":"address"},{"indexed":true,"internalType":"address","name":"_to","type":"address"},{"indexed":true,"internalType":"uint256","name":"_tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"CANNOT_TRANSFER_TO_ZERO_ADDRESS","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"NOT_CURRENT_OWNER","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_approved","type":"address"},{"internalType":"uint256","name":"_tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_owner","type":"address"},{"internalType":"address","name":"_operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_to","type":"address"},{"internalType":"uint256","name":"_tokenId","type":"uint256"},{"internalType":"string","name":"_uri","type":"string"}],"name":"mint","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"_name","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"_owner","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_from","type":"address"},{"internalType":"address","name":"_to","type":"address"},{"internalType":"uint256","name":"_tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_from","type":"address"},{"internalType":"address","name":"_to","type":"address"},{"internalType":"uint256","name":"_tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_operator","type":"address"},{"internalType":"bool","name":"_approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"_interfaceID","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"_symbol","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_from","type":"address"},{"internalType":"address","name":"_to","type":"address"},{"internalType":"uint256","name":"_tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}]')
+Contract = web3.eth.contract(address=contract_address, abi=abi)
+
+# Get the number of latest transaction
+nonce = web3.eth.getTransactionCount(address)
+
+#Get balance account
+print('')
+def UpdateBalance():
+    balance = web3.eth.get_balance(address)
+    balance_bnb = web3.fromWei(balance,'ether')
+    print('Your Balance' ,balance_bnb, 'ETH/BNB/MATIC/OTHER')
+    
+UpdateBalance()
+
+#estimate gas limit contract
+gas_tx = Contract.functions.mint(recipient, token_id, uri_url).buildTransaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": web3.eth.gas_price,
+        "from": address,
+        "nonce": nonce
+    }
+)
+gasAmount = web3.eth.estimateGas(gas_tx)
+
+#calculate transaction fee
+print('')
+gasPrice = web3.fromWei(web3.eth.gas_price, 'gwei')
+Caclfee = web3.fromWei(gasPrice*gasAmount, 'gwei')
+print('Transaction Fee :' ,Caclfee, 'ETH/BNB/MATIC/OTHER')
+
+# build transaction
+transaction = Contract.functions.mint(recipient, token_id, uri_url).buildTransaction(
+    {
+        "chainId": chain_id,
+        "gas": gasAmount,
+        "gasPrice": web3.eth.gas_price,
+        "from": address,
+        "nonce": nonce
+    }
+)
+# Sign the transaction
+sign_transaction = web3.eth.account.sign_transaction(transaction, private_key)
+# Send the transaction
+transaction_hash = web3.eth.send_raw_transaction(sign_transaction.rawTransaction)
+# Wait for the transaction to be mined, and get the transaction receipt
+#get transaction hash
+txid = str(web3.toHex(transaction_hash))
+print('')
+print('Transaction Success TX-ID Copied To Clipboard')
+print(txid)
+pc.copy(txid)
+print('Update Current Balance In 30 Second...')
+time.sleep(30)
+print('')
+UpdateBalance() #get latest balance
+print('Will Close Automatically In 30 Second...')
+time.sleep(30)
