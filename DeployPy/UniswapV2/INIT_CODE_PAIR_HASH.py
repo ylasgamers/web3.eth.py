0e49ea3c0aedf5f990e5e68d4fa73eadc154844a
ylasgamers <92687144+ylasgamers@users.noreply.github.com>
2023-08-25 19:59:47 +0700

    Add files via upload

---
+++ DeployPy/UniswapV2/INIT_CODE_PAIR_HASH.py
@@ -0,0 +1,37 @@
+from web3 import Web3, HTTPProvider
+import json
+import logging
+import datetime 
+import threading
+import asyncio
+import requests
+import time
+import os
+import sys
+import ctypes
+import pyperclip as pc
+
+#bsc = "https://bsc-testnet.publicnode.com" #rpc bsctesnet custom #you can find rpc on chainlist.org
+inputrpc = str(input("Input Url RPC/Node Blockchain Network : "))
+web3 = Web3(Web3.HTTPProvider(inputrpc))
+#chain_id = 97 #you can find chainid on chainlist.org
+chainId = int(input("Input Chain ID Blockchain Network : "))
+
+#connecting web3
+if  web3.isConnected() == True:
+    print("Web3 Connected...\n")
+else :
+    print("Error Connecting Please Try Again...")
+
+contract_address = web3.toChecksumAddress(input("Enter Contract Address Factory : ")) #factory
+
+abi = json.loads('[{"inputs":[{"internalType":"address","name":"_feeToSetter","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"token0","type":"address"},{"indexed":true,"internalType":"address","name":"token1","type":"address"},{"indexed":false,"internalType":"address","name":"pair","type":"address"},{"indexed":false,"internalType":"uint256","name":"","type":"uint256"}],"name":"PairCreated","type":"event"},{"constant":true,"inputs":[],"name":"INIT_CODE_PAIR_HASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"allPairs","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"allPairsLength","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"tokenA","type":"address"},{"internalType":"address","name":"tokenB","type":"address"}],"name":"createPair","outputs":[{"internalType":"address","name":"pair","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"feeTo","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"feeToSetter","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"getPair","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_feeTo","type":"address"}],"name":"setFeeTo","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_feeToSetter","type":"address"}],"name":"setFeeToSetter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]')
+contract = web3.eth.contract(address=contract_address, abi=abi)
+INIT_CODE_PAIR_HASH = contract.functions.INIT_CODE_PAIR_HASH().call()
+INIT_CODE_PAIR_HASH_ID = str(web3.toHex(INIT_CODE_PAIR_HASH))
+print('')
+print('INIT_CODE_PAIR_HASH Copied To Clipboard')
+print(INIT_CODE_PAIR_HASH_ID)
+pc.copy(INIT_CODE_PAIR_HASH_ID)
+print('Will Close Automatically In 30 Second...')
+time.sleep(30)
