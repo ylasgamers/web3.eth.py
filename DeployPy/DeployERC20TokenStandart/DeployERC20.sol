1383f0a79c78083ae6605e536896e8f38ffb2dec
ylasgamers <92687144+ylasgamers@users.noreply.github.com>
2023-10-04 23:09:08 +0700

    Update DeployERC20.sol

--- DeployPy/DeployERC20TokenStandart/DeployERC20.sol
+++ DeployPy/DeployERC20TokenStandart/DeployERC20.sol
@@ -1,6 +1,5 @@
 //SPDX-License-Identifier: MIT
-
-pragma solidity =0.8.21;
+pragma solidity =0.8.19;
 
 abstract contract Context {
     function _msgSender() internal view virtual returns (address) {
