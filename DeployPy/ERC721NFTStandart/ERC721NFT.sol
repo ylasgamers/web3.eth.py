5aa0a95a3ae0915d380ead3d9e5aceb997b9666d
ylasgamers <92687144+ylasgamers@users.noreply.github.com>
2023-10-04 23:05:24 +0700

    Update ERC721NFT.sol

--- DeployPy/ERC721NFTStandart/ERC721NFT.sol
+++ DeployPy/ERC721NFTStandart/ERC721NFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.21;
+pragma solidity =0.8.19;
 
 interface ERC721 {
     event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
