f5800683002c62f5b90a75f7c3d684049758c468
ylasgamers <92687144+ylasgamers@users.noreply.github.com>
2023-12-26 17:34:36 +0700

    Update sendtx.py

--- Solana/SendTransaction/sendtx.py
+++ Solana/SendTransaction/sendtx.py
@@ -1,5 +1,7 @@
 from solathon.core.instructions import transfer
 from solathon import Client, Transaction, PublicKey, Keypair
+from solathon.utils import clean_response, lamport_to_sol, sol_to_lamport
+import time
 
 #client = Client("https://rpc.solscan.com") #mainnet
 #client = Client("https://api.testnet.solana.com") #testnetㅤ
@@ -9,10 +11,16 @@ client = Client("https://api.devnet.solana.com") #devnet
 sender = PublicKey(input("Input Your Publickey/Address Sol : "))
 keysender = Keypair.from_private_key(input("Input Your Privatekey : "))
 receiver = PublicKey(input("Input Receiver Publickey/Address Sol : "))
-value = float(input("Input Amount Of You Want To Send : ")) * int(1000000000)
+inputval = float(input("Input Amount Of You Want To Send : "))
+value = sol_to_lamport(inputval)
 
-balance = client.get_balance(sender)
-print("Your Balance : ",balance)
+def GetBalance():
+    print("")
+    balance = client.get_balance(sender)
+    tosol = lamport_to_sol(clean_response(balance))
+    print("Your Balance : ",tosol, "SOL")
+    print("")
+GetBalance()
 
 instruction = transfer(
     from_public_key=keysender.public_key,
@@ -23,4 +31,4 @@ instruction = transfer(
 transaction = Transaction(instructions=[instruction], signers=[keysender])
 
 result = client.send_transaction(transaction)
-print("Transaction Respons : ", result)
+print("Transaction ID : ", clean_response(result))
