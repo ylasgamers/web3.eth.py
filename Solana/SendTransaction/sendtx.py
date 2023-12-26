de6f493a7de9d979de0cbbf4fd7c676028916fe5
ylasgamers <92687144+ylasgamers@users.noreply.github.com>
2023-12-26 16:30:25 +0700

    Update sendtx.py

--- Solana/SendTransaction/sendtx.py
+++ Solana/SendTransaction/sendtx.py
@@ -6,16 +6,21 @@ from solathon import Client, Transaction, PublicKey, Keypair
 client = Client("https://api.devnet.solana.com") #devnet
 
 # Requires you to have some SOLs to pay for transaction fees
-sender = Keypair.from_private_key(input("Input Your Privatekey : "))
-receiver = PublicKey(input("Input Your Publickey/Address Sol : "))
+sender = PublicKey(input("Input Your Publickey/Address Sol : "))
+keysender = Keypair.from_private_key(input("Input Your Privatekey : "))
+receiver = PublicKey(input("Input Receiver Publickey/Address Sol : "))
+value = float(input("Input Amount Of You Want To Send : ")) * int(1000000000)
+
+balance = client.get_balance(sender)
+print("Your Balance : ",balance)
 
 instruction = transfer(
-    from_public_key=sender.public_key,
+    from_public_key=keysender.public_key,
     to_public_key=receiver, 
-    lamports=10000000 #https://www.solconverter.com/
+    lamports=int(value) #https://www.solconverter.com/
 )
 
-transaction = Transaction(instructions=[instruction], signers=[sender])
+transaction = Transaction(instructions=[instruction], signers=[keysender])
 
 result = client.send_transaction(transaction)
-print("Transaction response: ", result)
+print("Transaction Respons : ", result)
